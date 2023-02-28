0b7894441d8832ca580d7a79dfafdd123152e1a8
aleksandr_gontarev <gontarevsanya@gmail.com>
2023-02-28 21:11:27 +0200

    fix js dark

--- app/js/scriptdark.js
+++ app/js/scriptdark.js
@@ -3,9 +3,8 @@ const burger=document.querySelector('.header-menu__mobile-menu-list')
 const burger_item=document.querySelector('.header-menu__list-item')
 const body=document.querySelector('.body')
 
-
 buton.addEventListener('click', function() {
-    buton.classList.toggle('active-btn')
+    buton.classList.toggle('active-btn-dark')
     burger.classList.toggle('header-menu__mobile-menu-list-show')
     burger.classList.toggle('show')
     burger_item.classList.toggle('header-menu__list-item-show')
